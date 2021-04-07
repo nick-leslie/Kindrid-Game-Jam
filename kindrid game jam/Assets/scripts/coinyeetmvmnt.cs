9a10f26b909097df37a70e5f6ced4996b8ad3a5a
jGraham760 <72208867+jGraham760@users.noreply.github.com>
2021-04-07 16:13:31 -0600

    Added turret
    YEAH BOIIII

---
+++ kindrid game jam/Assets/scripts/coinyeetmvmnt.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinyeetmvmnt : MonoBehaviour
+{
+    public float SPEED = 5F;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(SPEED * Time.deltaTime, 0, 0);
+    }
+
+    void OnTriggerStay2D(Collider2D frank)
+    {
+        Destroy(gameObject);
+        Debug.Log("hello");
+    }
+}
+
